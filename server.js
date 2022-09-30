15658f91b9c9417c6d7af3778fcc1d04481606f2
Ruben <rubendc91@gmail.com>
2022-09-29 18:14:11 -0700

    compleated code

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const htmlRoute= require("./routes/htmlRoute")
+const apiRoute= require("./routes/apiRoute")
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use("/", htmlRoute);
+app.use("/api", apiRoute);
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
